8a669d3e0b38cdb8daf7be2d4a6971c8e20dd9aa
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-23 00:07:28 +0100

    Main section'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import MainHeader from "./components/Header/MainHeader";
 import SectionFeature from "./components/SectionFeature/SectionFeature";
+import SectionMain from "./components/MainSection/SectionMain";
 
 function App() {
   return (
     <div className="">
       <MainHeader />
       <SectionFeature />
+      <SectionMain />
     </div>
   );
 }
---
+++ src/components/MainSection/SectionMain.jsx
@@ -0,0 +1,12 @@
+import classes from './SectionMain.module.css'
+
+const SectionMain = () => {
+    return (
+        <section className={classes.section_main}>
+            <h2>Insperation for your next adventure</h2>
+        </section>
+        
+    )
+}
+
+export default SectionMain
