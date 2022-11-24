73d1258a216e3da242c2e11b2817e71ac715fedc
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-25 00:03:10 +0100

    Use createPortal to change position of backdrop and overlay

--- src/components/Wallet-connect/WalletConnect.jsx
+++ src/components/Wallet-connect/WalletConnect.jsx
@@ -1,27 +1,34 @@
 import React from "react";
+import ReactDOM  from "react-dom";
 import walletFrame from "../../Assets/Frame 37.png";
 import classes from "./WallectConnet.module.css";
 
 const WalletConnect = (props) => {
-  //   const Backdrop = () => {
-  //     return <div className={classes.backdrop} ></div>;
-  //   };
-  {
-    /* {ReactDOM.createPortal(
-    <Backdrop onClear={props.onClear} />,
-    document.getElementById("backdrop-root")
-  )} */
-  }
+  const Backdrop = () => {
+    return <div className={classes.backdrop} onClick={props.onRemove} />;
+  };
 
-  return (
-    <>
-      <div className={classes.backdrop} onClick={props.onRemove} />
+  const ModalOverlay = () => {
+    return (
       <img
         src={walletFrame}
         alt="Frame showing wallet to connect"
         className={classes.modal}
         onClick={props.onRemove}
       />
+    );
+  };
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onRemove} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay />,
+        document.getElementById("modalOverly-root")
+      )}
     </>
   );
 };
